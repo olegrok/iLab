246b6b0c21539c2d953b5abb318f79eb715ac386
olegrok <babinoleg@mail.ru>
2014-10-23 12:56:50 +0400

    Create stacktitle.h

---
+++ 03----Stack/stacktitle.h
@@ -0,0 +1,7 @@
+#include "stacklib.h"
+struct stack_t* stack_construct(int stack_size);
+int destruct_stack(struct stack_t* Stack);
+int stack_push(struct stack_t* Stack, int value);
+int stack_top(struct stack_t* Stack);
+int stack_pop(struct stack_t* Stack);
+void stack_dump(const struct stack_t* Stack);
