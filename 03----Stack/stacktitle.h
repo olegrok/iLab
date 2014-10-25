b8656b98a8ce4f09de924e9eb1262c421ad1a71e
olegrok <babinoleg@mail.ru>
2014-10-25 17:24:35 +0400

    Update stacktitle.h

--- 03----Stack/stacktitle.h
+++ 03----Stack/stacktitle.h
@@ -1,7 +1,10 @@
 #include "stacklib.h"
+#include "assembler.h"
 struct stack_t* stack_construct(int stack_size);
 int destruct_stack(struct stack_t* Stack);
 int stack_push(struct stack_t* Stack, int value);
 int stack_top(struct stack_t* Stack);
 int stack_pop(struct stack_t* Stack);
 void stack_dump(const struct stack_t* Stack);
+void assembler();
+void disassembler();
