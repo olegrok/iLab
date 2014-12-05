a2f979b1f83a3853c6b43bde11f62de3b0aa34e6
olegrok <babinoleg@mail.ru>
2014-12-05 12:23:29 +0300

    Update Hash_table.h

--- 04------Hash_table/Hash_table.h
+++ 04------Hash_table/Hash_table.h
@@ -1,21 +1,31 @@
 #define _NOT_FOUND_ -1
+
 struct Table
 {
     int count;
     unsigned int(*Funct)(char* str);
     struct list* hash_table;
 };
+
+
 struct list* table_massive_ctor(int count)
 {
     struct list* hash_table = new_list(count);
     return hash_table;
 }
+
+
+
+
 void massive_table_dtor(struct list* hash_table, int count)
 {
     delete_hash_table(hash_table, count);
 }
+
+
 struct Table* table_ctor(int count, unsigned int (*function(char *str)))
 {
+    assert(function);
     struct Table * ht = calloc(1, sizeof(struct Table));
     ht -> count = count;
     ht -> Funct = function;
@@ -23,17 +33,22 @@ struct Table* table_ctor(int count, unsigned int (*function(char *str)))
     return ht;
 
 }
+
+
 void table_dtor(struct Table* table)
 {
+    assert(table);
     struct list* hash_table = table -> hash_table;
     int count = table -> count;
     massive_table_dtor(hash_table, count);
     free(table);
-
 }
+
+
 int table_search(struct Table* table, char* str)
 {
     assert(table);
+    assert(str);
     struct list* hash_table = table -> hash_table;
     int count = table -> count;
     unsigned int hash = 0;
@@ -44,9 +59,12 @@ int table_search(struct Table* table, char* str)
     }
     return hash;
 }
+
+
 int table_add(struct Table* table, char* str)
 {
     assert(table);
+    assert(str);
     struct list* hash_table = table -> hash_table;
     int count = table -> count;
     unsigned int hash = 0;
@@ -57,6 +75,8 @@ int table_add(struct Table* table, char* str)
     assert(table);
     return 1;
 }
+
+
 void print_hash_massive(struct list *l, int count)
 {
     assert(l);
@@ -67,17 +87,23 @@ void print_hash_massive(struct list *l, int count)
         print_list(&l[i]);
     }
 }
+
+
 void print_table(struct Table* table)
 {
+    assert(table);
     struct list* hash_table = table -> hash_table;
     int count = table -> count;
     print_hash_massive(hash_table, count);
     table = NULL;
 
 }
+
+
 int table_remove_str(struct Table* table, char* str)
 {
     assert(table);
+    assert(str);
     struct list* hash_table = table -> hash_table;
     int count = table -> count;
     assert(hash_table);
@@ -85,8 +111,6 @@ int table_remove_str(struct Table* table, char* str)
     hash = abs(table -> Funct(str));
     struct list* l = &hash_table[hash % count];
     struct li *it = NULL;
-    struct li *buf = NULL;
-    struct li *prev = l -> first;
     if(list_find(l, str))
     {
        for (it = l -> first; it != NULL; it = it -> next)
@@ -96,16 +120,19 @@ int table_remove_str(struct Table* table, char* str)
                 strcpy(it -> value, "");
                 it -> key = 0;
                 l -> size--;
+                assert(table);
                 return 1;
             }
         }
-        assert(table);
     }
     assert(table);
     return _NOT_FOUND_;
 }
+
+
 void fill_table(struct Table* table, FILE* file)
 {
+    assert(table);
     unsigned int hash = 0, i = 0;
     struct list* hash_table = table -> hash_table;
     int count = table -> count;
@@ -133,5 +160,4 @@ void fill_table(struct Table* table, FILE* file)
         fprintf(results, "%d, ", hash_table[i].size);
     fprintf(results, "%s", "\n");
     fclose(results);
-
 }
