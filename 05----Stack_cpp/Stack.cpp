5f447d9785cf459cf0ffcc457d04c77f76cf36a3
olegrok <babinoleg@mail.ru>
2015-03-06 18:59:05 +0300

    Update Stack.cpp

--- 05----Stack_cpp/Stack.cpp
+++ 05----Stack_cpp/Stack.cpp
@@ -13,10 +13,14 @@ class Stack
         ~Stack();
         bool push(stack_t val);
         stack_t pop();
-        bool dump() const;
+        void dump() const;
+        bool StOk() const;
+        void StAssert() const;
+        bool StCheck() const;
+        stack_t peek() const;
 
 
-    private:
+    //private:
         stack_t* data;
         int count;
         int max;
@@ -30,11 +34,8 @@ Stack::Stack():
     max(10)
     {
         std::cout << "Created stack " << max << " elements" << std::endl;
+        StCheck();
     }
-
-
-
-
 
 Stack::Stack(int size):
     data((stack_t*)calloc(size, sizeof(stack_t))),
@@ -44,50 +45,148 @@ Stack::Stack(int size):
         std::cout << "Created stack " << max << " elements" << std::endl;
     }
 
-
 Stack::~Stack()
 {
-    std::cout << "Deleted stack " << max << " elements" << std::endl;
-    free(data);
-    data = NULL;
-    count = -1;
-    max = -1;
+    if(StOk())
+    {
+        std::cout << "Deleted stack " << max << " elements" << std::endl;
+        free(data);
+        data = NULL;
+        count = -1;
+        max = -1;
+    }
+    else
+        std::cout << "Invalid stack. Did not delete" << std::endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+void Stack::dump() const
+{
+    std::cout << "\n\n";
+    std::cout << "\tStack dump" << std::endl;
+    std::cout << "\tData: " << data <<std:: endl;
+
+    std::cout << "\tCount: ";
+    if(StOk())
+         std::cout << count <<std:: endl;
+    else
+        std::cout << "?" <<std:: endl;
+
+    std::cout << "\tMax: ";
+    if(StOk())
+         std::cout << max <<std:: endl;
+    else
+        std::cout << "?" <<std:: endl;
+    std::cout << "\n\n";
+
+    if(StOk())
+        for(int i = 0; i < max; i++)
+        {
+            std::cout <<"\t[" << i << "]" << " = " << data[i] << " ";
+            if(i < count)
+                std::cout << "*\n";
+            else
+                std::cout << "\n";
+        }
+    std::cout << "\n\n";
+}
+bool Stack::StOk() const
+{
+    if(this && this -> data && max > 0)
+        return 1;
+    else
+        return 0;
+}
+
+void Stack::StAssert() const
+{
+    dump();
+    assert(StOk());
+}
+
+bool Stack::StCheck() const
+{
+    if(StOk())
+        return 1;
+    else
+        StAssert();
+    return 0;
 }
 
 
 bool Stack::push(stack_t val)
 {
-    //!!!
-    if(count == (max - 1))
+    StCheck();
+    if(count == max)
     {
-        data = (stack_t*)realloc(data, sizeof(stack_t) * (max + 5));
+        data = (stack_t*)realloc(data, sizeof(stack_t) * (max + 4));
+        max += 4;
     }
 
 
     data[count++] = val;
+
+    StCheck();
     return 1;
 }
 
 
 stack_t Stack::pop()
 {
-
+    StCheck();
+    if(count == 0)
+    {
+        std::cout << "\nStack is empty!\n Returned NULL\n";
+        return NULL;
+    }
     return data[--count];
 }
+
+stack_t Stack::peek() const
+{
+    StCheck();
+    if(count == 0)
+    {
+        std::cout << "\nStack is empty!\n Returned NULL\n";
+        return NULL;
+    }
+    return data[count - 1];
+}
+
 
 
 int main()
 {
-    Stack s1(5);
+    Stack s1(2);
     Stack s2;
     s1.push(5);
-    s1.push(5);
-    s1.push(5);
-    s1.push(5);
-    s1.push(5);
     s1.push(1);
-    s1.push(2);
-
+    //s1.push(2);
+    //s1.push(2);
+    //s1.push(2);
+    //s1.push(2);
+    //s1.push(0);
+    std::cout << s1.peek() << "\n";
+    std::cout << s1.peek() << "\n";
+    std::cout << s1.peek() << "\n";
+    std::cout << s1.pop() << "\n";
+    std::cout << s1.pop() << "\n";
+    std::cout << s1.pop() << "\n";
+    std::cout << s1.pop() << "\n";
+    std::cout << s1.peek() << "\n";
+    std::cout << s1.peek() << "\n";
+    //s1.data = 0;
+    s1.StAssert();
 
     return 0;
 }
